173f1ee5e3b6d3f5347af7ac9a58c4762ad85385
kalebeccs <kalebe.correa.santos@gmail.com>
2022-07-21 20:21:24 +0100

    Update login.js
    adicionando import para a funcao de validação e listener nos inputs.

--- adopet/assets/js/login.js
+++ adopet/assets/js/login.js
@@ -1,4 +1,11 @@
 import { inputFocusBorder, showHiddenInput } from "./inputSenha.js"
+import { valida } from "./validaLogin.js"
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input => {
+    input.addEventListener('blur', event => valida(event.target))
+})
 
 inputFocusBorder('input-senha-login')
 showHiddenInput('input-senha-login', 'input-icon-login')
